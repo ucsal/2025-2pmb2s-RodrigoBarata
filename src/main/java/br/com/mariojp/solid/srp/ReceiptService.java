c557efce00ae463e67a243616ee65af6c718c676
Your Name <you@example.com>
2025-08-21 09:32:05 -0300

    feat: cria classe TaxCalculator para calcular imposto configurável via System property

--- src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
+++ src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
@@ -1,19 +1,20 @@
 package br.com.mariojp.solid.srp;
-
+import java.util.List;
 public class ReceiptService {
-	public String generate(Order order) {
-		double subtotal = order.getItems().stream().mapToDouble(i -> i.getUnitPrice() * i.getQuantity()).sum();
-		double tax = subtotal * 0.10; //Taxa 10 fixa :(
-		double total = subtotal + tax;
-		StringBuilder sb = new StringBuilder(); //Formatando o Recibo
-		sb.append("=== RECIBO ===\n");
-		for (var i : order.getItems()) {
-			sb.append(i.getName()).append(" x").append(i.getQuantity()).append(" = ").append(i.getUnitPrice() * i.getQuantity())
-					.append("\n");
-		}
-		sb.append("Subtotal: ").append(subtotal).append("\n");
-		sb.append("Tax: ").append(tax).append("\n");
-		sb.append("Total: ").append(total).append("\n");
-		return sb.toString();
+	private final  TaxCalculador taxCalculador;
+    private final ReceiptFomatter receiptFomatter;
+
+
+
+    public ReceiptFomatter(TaxCalculador taxCalculador,ReceiptFomatter receiptFomatter){
+		this.taxCalculador=taxCalculador;
+        this.receiptFomatter=receiptFomatter;
 	}
-}
+	public String generateReceipt(List<Double> items){
+		double subtotal=items.stream().mapToDouble(Double:: doubleValue).sum();
+		double tax= taxCalculador.calculartax(subtotal);
+		double total= subtotal+tax;
+		return receiptFomatter.format(subtotal, tax, total);
+	}
+	}
+
